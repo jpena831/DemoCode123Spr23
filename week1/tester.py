c28939cecaad77f2bb2c7fe73e2beee0585e7bc8
Joslenne <joslennepena@gmail.com>
2023-01-23 20:30:51 -0600

    tester.py

---
+++ week1/tester.py
@@ -0,0 +1,15 @@
+# 1/23/23
+# this is a single comment using a pound sign, it is gray
+# Joslenne
+# write your acknowledgements, like those who helped you
+
+""" this is a multiline comment in Python, it's green and Python does not execute
+comments"""
+
+name = "joslenne" # this is a string inside a variable which stores data for us
+
+num1 = 5 # this variable holds an integer
+
+print("hello world") # just printed a string without using a variable directly in print statement
+print(name)
+print(num1)
