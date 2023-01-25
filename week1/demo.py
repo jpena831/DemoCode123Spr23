e3a0962d6d77759c40c9d9230e511e65cad8d05b
Joslenne <joslennepena@gmail.com>
2023-01-25 15:41:40 -0600

    from class 1/25/23

---
+++ week1/demo.py
@@ -0,0 +1,53 @@
+# string - my name is joslenne
+# function print()
+# print(name)
+
+# below are 3 different ways to get the output value 8
+y = 4
+z = 4
+finaltotal = y + z
+
+print(finaltotal)
+
+print(y + z)
+
+print(4 + 4)
+
+city = "minneapolis"
+state = "minnesota"
+
+print(city + " " + state)
+print(city, " ", state)
+
+print(100 - 20)  # subtraction
+print(50 * 20)  # multiplication
+print(100 / 20)  # division
+print(4 ** 2)  # exponent
+print(100 % 27)  # modulus operator
+
+a = 22  # assignment operator constitutes 1 equal sign
+b = 22
+
+print(a == b)  # double equal signs actually compare the value to test whether something is truly equal or not
+
+j = 45
+m = 50
+
+m = m + 2  # we can update variables from their original value assignment whether that is number or text
+print(m)
+
+# using input prompt to write a string and store potential answer
+question = input("Please enter your nickname: ")
+print("Hello", question)
+
+num1 = 4
+print(type(200))  # the type function will tell you the exact data type interpreted by Python
+print(type(0.005))
+print(type("today is the eighth"))
+
+# PEMDAS is a rule followed in computing solutions in python
+print(10 + 20 / 10)
+
+""" This is a way
+to make longer multi-line 
+comments """
